89714178ceee9e0de081e8d94936bde9c48483c1
singuyen0312e9 <singuyen090807@gmail.com>
2025-06-18 08:39:02 +0700

    [add] lab-03 rsa - ecc

--- Lab-03/cipher/ecc/ecc_cipher.py
+++ Lab-03/cipher/ecc/ecc_cipher.py
@@ -1,75 +1,38 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
-from ui.ecc import Ui_MainWindow
-import requests
+import ecdsa, os
 
-class MyApp(QMainWindow):
+if not os.path.exists('cipher/ecc/keys'):
+    os.makedirs('cipher/ecc/keys')
+
+class ECCCipher:
     def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.btn_generatekey.clicked.connect(self.call_api_gen_keys)
-        self.ui.btn_sign.clicked.connect(self.call_api_sign)
-        self.ui.btn_verify.clicked.connect(self.call_api_verify)
+        pass
 
-    def call_api_gen_keys(self):
-        url = "http://127.0.0.1:5000/api/ecc/generate_keys"
+    def generate_keys(self):
+        sk = ecdsa.SigningKey.generate()  # Tạo khóa riêng tư
+        vk = sk.get_verifying_key()       # Lấy khóa công khai từ khóa riêng tư
+
+        with open('cipher/ecc/keys/privateKey.pem', 'wb') as p:
+            p.write(sk.to_pem())
+
+        with open('cipher/ecc/keys/publicKey.pem', 'wb') as p:
+            p.write(vk.to_pem())
+
+    def load_keys(self):
+        with open('cipher/ecc/keys/privateKey.pem', 'rb') as p:
+            sk = ecdsa.SigningKey.from_pem(p.read())
+
+        with open('cipher/ecc/keys/publicKey.pem', 'rb') as p:
+            vk = ecdsa.VerifyingKey.from_pem(p.read())
+
+        return sk, vk
+
+    def sign(self, message, key):
+        # Ký dữ liệu bằng khóa riêng tư
+        return key.sign(message.encode('ascii'))
+
+    def verify(self, message, signature, key):
+        _, vk = self.load_keys()
         try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                data = response.json()
-                msg = QMessageBox()
-                msg.setIcon(QMessageBox.Information)  # Sửa lại chữ hoa đầu
-                msg.setText(data["message"])
-                msg.exec_()
-            else:
-                print("Error while calling API")
-        except requests.exceptions.RequestException as e:
-            print("Error: %s" % e)
-
-    def call_api_sign(self):
-        url = "http://127.0.0.1:5000/api/ecc/sign"
-        payload = {
-            "message": self.ui.txt_information.toPlainText(),
-        }
-        try:
-            response = requests.post(url, json=payload)
-            if response.status_code == 200:
-                data = response.json()
-                self.ui.txt_signature.setText(data["signature"])
-                msg = QMessageBox()
-                msg.setIcon(QMessageBox.Information)
-                msg.setText("Signed Successfully")
-                msg.exec_()
-            else:
-                print("Error while calling API")
-        except requests.exceptions.RequestException as e:
-            print("Error: %s" % e)
-
-    def call_api_verify(self):
-        url = "http://127.0.0.1:5000/api/ecc/verify"
-        payload = {
-            "message": self.ui.txt_information.toPlainText(),
-            "signature": self.ui.txt_signature.toPlainText()
-        }
-        try:
-            response = requests.post(url, json=payload)
-            if response.status_code == 200:
-                data = response.json()
-                msg = QMessageBox()
-                msg.setIcon(QMessageBox.Information)
-                if data["is_verified"]:
-                    msg.setText("Verified Successfully")
-                else:
-                    msg.setText("Verified Fail")
-                msg.exec_()
-            else:
-                print("Error while calling API")
-        except requests.exceptions.RequestException as e:
-            print("Error: %s" % e)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MyApp()
-    window.show()
-    sys.exit(app.exec_())
+            return vk.verify(signature, message.encode('ascii'))
+        except ecdsa.BadSignatureError:
+            return False
